f9a2a745300c0a4895e7e991d65054e571cc5bd8
miShelbyT <mi.shelbyrose@gmail.com>
2021-05-17 11:15:36 -0400

    set up side drawer with hyperlinks and fixed formatting issues

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import clsx from 'clsx'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Drawer from '@material-ui/core/Drawer'
-// import Link from '@material-ui/core/Link'
+import Link from '@material-ui/core/Link'
 import Typography from '@material-ui/core/Typography'
 
 import Divider from '@material-ui/core/Divider'
@@ -20,8 +20,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
   },
   drawerLinks: {
-    marginLeft: 25,
-    marginTop: 8
+    margin: '27px 18px'
   },
   drawerOpen: {
     backgroundColor: '#bdbdbd',
@@ -45,9 +44,11 @@ const useStyles = makeStyles((theme) => ({
   },
   teal: {
     backgroundColor: 'rgba(0, 150, 135, 0.8)',
+    margin: 20
   },
   blue: {
     backgroundColor: 'rgba(3, 169, 244, 0.8)',
+    margin: 20
   },
 }))
 
@@ -83,36 +84,32 @@ function SideBar({ handleDrawerClose, open }) {
         <div style={{ marginTop: 30 }}>
           <div className={'options'}>
             <Avatar className={classes.blue} variant='rounded'>A</Avatar>
+            <Link href="/about" underline='none'>
             <Typography className={classes.drawerLinks}>About</Typography>
+            </Link>
           </div>
 
           <div className={'options'}>
             <Avatar className={classes.teal}>P</Avatar>
+            <Link href="/projects" underline='none'>
             <Typography className={classes.drawerLinks}>Projects</Typography>
+            </Link>
           </div>
 
           <div className={'options'}>
             <Avatar className={classes.blue} variant='rounded'>B</Avatar>
+            <Link href="/blogs" underline='none'>
             <Typography className={classes.drawerLinks}>Blogs</Typography>
+            </Link>
           </div>
 
           <div className={'options'}>
             <Avatar className={classes.teal}>C</Avatar>
+            <Link href="/contact" underline='none'>
             <Typography className={classes.drawerLinks}>Contact</Typography>
+            </Link>
           </div>
 
-          {/* <Link href="/about" underline='none'>
-            <Typography className={classes.drawerLinks}>About</Typography>
-          </Link>
-          <Link href="/projects" underline='none'>
-            <Typography className={classes.drawerLinks}>Projects</Typography>
-          </Link>
-          <Link href="/blogs" underline='none'>
-            <Typography className={classes.drawerLinks}>Blogs</Typography>
-          </Link>
-          <Link href="/contact" underline='none'>
-            <Typography className={classes.drawerLinks}>Contact</Typography>
-          </Link> */}
         </div>
         <Divider />
       </Drawer>
