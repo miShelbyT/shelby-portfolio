e33048914e8c10d24667026dc6dac6724c75e06f
miShelbyT <mi.shelbyrose@gmail.com>
2021-05-19 14:08:03 -0400

    projects refactor complete

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -51,7 +51,9 @@ function Projects({ projectsRef }) {
   const classes = useStyles()
   const [expanded, setExpanded] = React.useState(false)
 
-  const nonDemoProjects = [{
+
+  const projects = [{
+    // no demo for these projects
     id: 1,
     image: cupcakes,
     title: "Brelby's Wine & Cupcakes",
@@ -59,23 +61,22 @@ function Projects({ projectsRef }) {
     link: "https://mishelbyt.github.io/brelbys-sass/practice-project/dist/",
     buttonText: "Brelby's as Deployed Via Github Pages",
     moreText: "Responsively designed, based on a SASS tutorial from CodeStackr. This app is lovingly adapted from a Flatiron School project created with my wonderful friend Bryn Knowles. Brelby's lives on!"
-  }]
-
-  const demoProjects = [{
-    id: 100,
-    src: "https://www.youtube.com/embed/Fhb1zT1z-WQ",
+  },
+  // demos for these projects
+  {id: 2,
+    youTubeSrc: "https://www.youtube.com/embed/Fhb1zT1z-WQ",
     title: "Take Care",
     subheader: "A journaling app that focuses on self-care/mental health",
     moreText: "Take Care is a self-care journaling app that allows the user to sign up/log in, create a profile, peruse a list of self-care activities, save, update and delete journal entries pertaining to those activities as well as create original activities. Also uses an external API to generate daily affirmations via a \"roulette\" affirmation button."
   }, 
-  { id: 101,
-    src: "https://www.youtube.com/embed/U-3CXBiqes4",
+  { id: 3,
+    youTubeSrc: "https://www.youtube.com/embed/U-3CXBiqes4",
     title: "Third Eye Tarot",
     subheader: "A tarot card app that invites playfulness and introspection",
     moreText: "Tarot Card App that encourages curiosity and insight into past, current and future life events. Third Eye Tarot allows the user to sign up/log in, \"consult the cards\" with a question, deal themselves a reading of between 1 and 5 tarot cards, record their comments/reflections on each reading and then save/review, update and delete card readings."
   },
-  { id: 102,
-    src: "https://www.youtube.com/embed/uolpe_CaOec",
+  { id: 4,
+    youTubeSrc: "https://www.youtube.com/embed/uolpe_CaOec",
     title: "Pet Dates",
     subheader: "A pet scheduling app",
     moreText: "Because pets need friends too! This pet-date scheduler app is currently being used by dogs, cats, a gecko and a stuffed raccoon."
@@ -95,71 +96,39 @@ function Projects({ projectsRef }) {
         justify="center"
         alignItems="center"
       >
-        {nonDemoProjects.map(project => (
-        
+        {projects.map(project => (
         <Grid item 
         key={project.id}
         sm={12} lg={6}>
           <Card className={classes.root}>
-            <CardMedia
+            {project.youTubeSrc ? ( <div>
+              <iframe
+                width="550"
+                height="315"
+                src={project.youTubeSrc}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>) : (<CardMedia
               className={classes.media}
               image={project.image}
               title={project.title}
-            />
+            />)}
 
             <CardHeader
               title={project.title}
               subheader={project.subheader}
             />
-            <Link
+            {project.link? (<Link
               href={project.link}
               underline="none"
             >
               <Button variant="outlined" color="secondary">
                 {project.buttonText}
               </Button>
-            </Link>
-            <CardActions disableSpacing>
-              <IconButton
-                className={clsx(classes.expand, {
-                  [classes.expandOpen]: expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-              >
-                <ExpandMoreIcon />
-              </IconButton>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-              <CardContent>
-                <Typography align="justify">
-                  {project.moreText}
-                </Typography>
-              </CardContent>
-            </Collapse>
-          </Card>
-        </Grid>
-        ))}
-
-        {demoProjects.map(project => (<Grid item sm={12} lg={6}>
-          <Card className={classes.root}>
-            <div>
-              <iframe
-                width="550"
-                height="315"
-                src={project.src}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
-
-            <CardHeader
-              title={project.title}
-              subheader={project.subheader}
-            />
+            </Link>) : null}
 
             <CardActions disableSpacing>
               <IconButton
