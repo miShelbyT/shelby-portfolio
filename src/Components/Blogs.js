87911bedb51f79253fae38a17ac71e5c97dff888
miShelbyT <mi.shelbyrose@gmail.com>
2021-08-15 10:40:33 -0400

    minor text fix

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -84,7 +84,7 @@ function Blogs() {
             <a href="https://levelup.gitconnected.com/">Level Up Coding</a>,{' '}
             <a href="https://blog.usejournal.com/">Noteworthy</a>,{' '}
             <a href="https://medium.com/an-idea">An Idea</a> and{' '}
-            <a href="https://medium.com/nerd-for-tech">Nerd For Tech's</a>{' '}
+            <a href="https://medium.com/nerd-for-tech">Nerd For Tech</a>{' '}
           </ListSubheader>
         </GridListTile>
         {myBlogs.map((blog) => (
