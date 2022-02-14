ad240f7053afc4af20f73ad034cbbf3befbd3e92
miShelbyT <mi.shelbyrose@gmail.com>
2022-02-14 17:17:06 -0500

    updated bloglist of publishers

--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -88,7 +88,6 @@ function Blogs() {
               Start Up/Geek Culture
             </a>,{' '}
             <a href="https://levelup.gitconnected.com/">Level Up Coding</a>,{' '}
-            <a href="https://blog.usejournal.com/">Noteworthy</a>,{' '}
             <a href="https://medium.com/an-idea">An Idea</a> and{' '}
             <a href="https://medium.com/nerd-for-tech">Nerd For Tech</a>{' '}
           </ListSubheader> : <ListSubheader
