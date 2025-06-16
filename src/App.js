137e7417c826555213cd76ea4329ced850acc03e
MiShelby Talbert <mi.shelbyrose@gmail.com>
2025-06-16 11:41:40 -0400

    Navigate not supported in rrd, using older syntax

--- src/App.js
+++ src/App.js
@@ -22,55 +22,54 @@ const theme = createTheme({
       //light grey
       main: '#faf7f9',
     },
-  }, typography: {
+  },
+  typography: {
     fontFamily: 'Poppins',
     fontSize: 10,
-  }
+  },
 })
 
 function App() {
-
-  
   return (
     <>
-    {/* <StyledEngineProvider injectFirst> */}
-    <BrowserRouter>
-    <ScrollToTop>
-      {/* with theme provider we can now provide theme props to entire app bc we've wrapped ThemeProvider around the rest of the app */}
-      <ThemeProvider theme={theme}>
-        <Main >
-          <Switch>
+      {/* <StyledEngineProvider injectFirst> */}
+      <BrowserRouter>
+        <ScrollToTop>
+          {/* with theme provider we can now provide theme props to entire app bc we've wrapped ThemeProvider around the rest of the app */}
+          <ThemeProvider theme={theme}>
+            <Main>
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
 
-          <Route exact path="/" >
-            <Home />
-          </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
 
-            <Route path="/about">
-              <About/>
-            </Route>
+                <Route path="/resume">
+                  <Resume />
+                </Route>
 
-            <Route path="/resume">
-              <Resume />
-            </Route>
+                <Route path="/projects">
+                  <Projects />
+                </Route>
 
-            <Route path="/projects">
-              <Projects/>
-            </Route>
+                <Route path="/blogs">
+                  <Blogs />
+                </Route>
 
-            <Route path="/blogs">
-              <Blogs/>
-            </Route>
+                <Route path="/contact">
+                  <Contact />
+                </Route>
 
-            <Route path="/contact">
-              <Contact/>
-            </Route>
-
-            <Route path="*" element={<Navigate to="/" replace />} />
-            
-          </Switch>
-        </Main>
-      </ThemeProvider>
-      </ScrollToTop>
+                <Route path="*">
+                  <Redirect to="/" />
+                </Route>
+              </Switch>
+            </Main>
+          </ThemeProvider>
+        </ScrollToTop>
       </BrowserRouter>
       {/* </StyledEngineProvider> */}
     </>
