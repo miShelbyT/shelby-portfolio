e72b04d5d440a213299f79b58be1f11597575c35
MiShelby Talbert <mi.shelbyrose@gmail.com>
2025-06-17 08:43:18 -0400

    fixing top margin on resume page

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import resume from '../resources/resume.pdf'
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(10)
+  }
+  }))
 
 function Resume() {
+
+  const classes = useStyles()
   return (
-    <div><iframe
+    <div className={classes.root}><iframe
       src={resume}
       width="600"
       height="800"
